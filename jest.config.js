2c9082271e88497d7ce062c78c547206e32c25f2
Alex Cesar <alexcesar.n@gmail.com>
2022-08-06 14:57:03 -0300

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
