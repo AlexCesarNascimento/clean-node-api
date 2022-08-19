baf56c79526ad668df2a880a467cca8f2334631a
Alex Cesar <alexcesar.n@gmail.com>
2022-08-19 20:34:17 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/SignUp/signUp.ts
+++ src/presentation/controllers/SignUp/signUp.ts
@@ -11,39 +11,38 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount
   }
   handle (httpRequest: HttpRequest): HttpResponse {
+     try{
+       const requiredFields = ['name', 'email', 'password','passwordConfirmation']
 
-   try{
-     const requiredFields = ['name', 'email', 'password','passwordConfirmation']
-
-     for (const field of requiredFields){
-       if (!httpRequest.body[field]){
-         return badRequest(new MissingParamError(field))
+       for (const field of requiredFields){
+         if (!httpRequest.body[field]){
+           return badRequest(new MissingParamError(field))
+         }
        }
+
+       const { name, email, password, passwordConfirmation} = httpRequest.body
+       if(password !== passwordConfirmation) {
+         return badRequest(new InvalidParamError('passwordConfirmation'));
+       }
+
+       const isValid = this.emailValidator.isValid(email)
+       if (!isValid) {
+         return badRequest(new InvalidParamError('email'))
+       }
+
+       const account = this.addAccount.add({
+         name,
+         email,
+         password
+       })
+
+       return {
+         statusCode: 200,
+         body: account
+       };
+
+     } catch (error) {
+       return serverError()
      }
-
-     const { name, email, password, passwordConfirmation} = httpRequest.body
-     if(password !== passwordConfirmation) {
-       return badRequest(new InvalidParamError('passwordConfirmation'));
-     }
-
-     const isValid = this.emailValidator.isValid(email)
-     if (!isValid) {
-       return badRequest(new InvalidParamError('email'))
-     }
-
-     this.addAccount.add({
-       name,
-       email,
-       password
-     })
-
-     return {
-       statusCode: 400,
-       body: new Error('Unidentified error')
-     };
-
-   } catch (error) {
-     return serverError()
-   }
   }
 }
